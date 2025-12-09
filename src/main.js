0bce70bb3f6c5f2d79974b8c0ebeecf0ad8d73fd
tungduc <duongduc201001@gmail.com>
2025-12-09 16:28:46 +0700

    first commit

---
+++ src/main.js
@@ -0,0 +1,12 @@
+//https://www.classicdriver.com/en
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+
+const app = createApp(App)
+
+app.use(router)
+
+app.mount('#app')
